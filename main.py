97c8d745a24fef00328cdc3b1252539c8f7f7bdb
JayoungKim <kjy95960125@gmail.com>
2026-02-14 17:59:22 +0900

    festival

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# 허용할 프론트엔드 주소 목록
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,           # 특정 도메인만 허용
+    allow_credentials=True,
+    allow_methods=["*"],             # 모든 HTTP 메서드(GET, POST 등) 허용
+    allow_headers=["*"],             # 모든 헤더 허용
+)
+
+@app.get("/")
+def home():
+    return {"message":"여기는 home입니다"}
+
+@app.get("/about")
+def about():
+    return {
+        "name": "김철수",
+        "phone": "010-123-4567",
+        "address": "서울시 종로구"
+    }   
+
+import random 
+
+# 랜덤 명언
+@app.get("/quote")
+def random_quote():
+    quotes = [
+        "성공은 준비된 자에게 찾아온다.",
+        "노력은 배신하지 않는다.",
+        "오늘 걷지 않으면 내일은 뛰어야 한다."
+    ]
+    return {"quote":random.choice(quotes)}
+
+# 랜덤 동물
+@app.get("/animal")
+def random_animal():
+    characteristics = ["귀여운", "용감한", "느긋한", "쏘 쿨한"]
+    animals = ["고양이", "강아지", "햄스터", "너구리"]   
+    return { "characteristic" : random.choice(characteristics), 
+            "animal" :random.choice(animals)} 
